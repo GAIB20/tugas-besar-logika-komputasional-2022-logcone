ed27d9c3afd8752ec0f0191e9e27dcacf3c4efd0
hanifmz07 <13521157@std.stei.itb.ac.id>
2022-11-15 20:06:10 +0700

    fix: fixing typo in rules name, remove Lokasi parameter

--- player.pl
+++ player.pl
@@ -7,10 +7,10 @@
 :- dynamic(count_pemain/3).
 
 /* rule pemain untuk informasi pemain secara keseluruhan */
-pemain(Nama, Lokasi, Indeks, Uang, Nilai_properti, Daftar_properti, Daftar_card, Count_jail, Count_double) :-
+pemain(Nama, Indeks, Uang, Nilai_properti, Daftar_properti, Daftar_card, Count_jail, Count_double) :-
     nama_pemain(Nama),
     aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti),
-    lokasi_pemain(Nama, Lokasi, Indeks),
+    lokasi_pemain(Nama, Indeks),
     card_pemain(Nama, Daftar_card),
     count_pemain(Nama, Count_jail, Count_double).
 
@@ -30,7 +30,7 @@ initPlayer :-
             (
                 asserta(aset_pemain(Nama, 1000, 0, [])),
                 asserta(card_pemain(Nama, [])),
-                asserta(lokasi_pemain(Nama, 'GO', 1)),
+                asserta(lokasi_pemain(Nama, 1)),
                 asserta(count_pemain(Nama, 0, 0))
             )
         )
@@ -38,17 +38,17 @@ initPlayer :-
 
 /* Display detail informasi pemain */
 checkPlayerDetail(Nama) :-
-    pemain(Nama, Lokasi, Uang, Nilai_properti, Daftar_properti, Daftar_card, Count_jail, Count_double),
+    pemain(Nama, Indeks, Uang, Nilai_properti, Daftar_properti, Daftar_card, Count_jail, Count_double),
     write('Informasi Player '), write(Nama), nl,
     nl,
-    write('Lokasi                       : '), write(Lokasi), nl,
+    write('Lokasi                       : '), write('Lokasi'), nl, % Lokasi added later 
     write('Total Uang                   : '), write(Uang), nl,
     write('Total Nilai Properti         : '), write(Nilai_properti), nl,
     Aset is Uang + Nilai_properti,
     write('Total Aset                   : '), write(Aset), nl,
     nl,
     write('Daftar Kepemilikan Properti  : '), nl,
-    displayProperti(Daftar_properti, 1), nl,
+    displayProperty(Daftar_properti, 1), nl,
     nl,
     write('Daftar Kepemilikan Card      : '), nl,
     displayCard(Daftar_card, 1), !.
@@ -56,7 +56,7 @@ checkPlayerDetail(Nama) :-
 /* Display properti pemain */
 displayProperty([], _) :- true.
 displayProperty([X|Tail], Count) :-
-    write(Count), write('. '), write(X), write(' - '), write('Kondisi'), nl,
+    write(Count), write('. '), write(X), write(' - '), write('Kondisi'), nl, % Kondisi (tanah, bangunan, landmark) added later
     Count_next is Count + 1,
     displayProperty(Tail, Count_next).
 
@@ -66,6 +66,10 @@ displayCard([X|Tail], Count) :-
     write(Count), write('. '), write(X), nl,
     Count_next is Count + 1,
     displayCard(Tail, Count_next).
+
+appendList([], A, [A]).
+appendList([X|Tail], A, [X|Rest]) :-
+    appendList(Tail, A, Rest).
 
 /* Reset data pemain */
 reset :-
