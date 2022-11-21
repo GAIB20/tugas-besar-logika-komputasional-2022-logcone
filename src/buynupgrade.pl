6d186a24ab7dde63f3308093f4df43960f482ec2
Ulung32 <13521122@mahasiswa.itb.ac.id>
2022-11-22 01:46:25 +0700

    add pay tax and world tour

--- src/buynupgrade.pl
+++ src/buynupgrade.pl
@@ -12,6 +12,24 @@ beli(nama) :-
     Tingkat =:= 2 -> propertyValue(ID, Buy0, Buy1, Buy2, _, _, _, _, _, _, _), aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti), UangNew is Uang - (Buy0 + Buy1 + Buy2), Nilai_properti_new is Nilai_properti + (Buy0 + Buy1 + Buy2), appendList(Daftar_properti, ID, Daftar_properti_new), aset_pemain(Nama, UangNew, Nilai_properti_new, Daftar_properti_new),
     Tingkat =:= 3 -> propertyValue(ID, Buy0, Buy1, Buy2, Buy3, _, _, _, _, _, _), aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti), UangNew is Uang - (Buy0 + Buy1 + Buy2 + Buy3), Nilai_properti_new is Nilai_properti +(Buy0 + Buy1 + Buy2 + Buy3), appendList(Daftar_properti, ID, Daftar_properti_new), aset_pemain(Nama, UangNew, Nilai_properti_new, Daftar_properti_new).
 
+
+payTax :-
+    aset_pemain(Pemilik, Uang, Nilai_properti, Daftar_properti),
+    Tax is 0.1*(Uang + Nilai_properti),
+    UangNew is Uang - Tax,
+    aset_pemain(Pemilik, UangNew, Nilai_properti, Daftar_properti).
+
+worldTour(Pemain) :-
+    wirte('Pilih indeks lokasi yang ingin kamu kunjungi : '),
+    read(Indeks),
+    lokasi_pemain(Pemain, Indeks),
+    write('Anda sudah sampai di lokasi tujuan'), nl,
+    Indeks >= 0, Indeks <= 24,
+    aset_pemain(Pemain, Uang, _, _),
+    UangNew is Uang + 10000,
+    aset_pemain(Pemain, UangNew, _, _),
+    write('Anda mendapat uang sebanyak XXX'), nl.
+
 % upgrade :-
 %     lokasi_pemain(Nama, Indeks),
 %     property(ID, _Nama_properti, Indeks, _Deskripsi_properti, _Tipe, _Rent, _Akuisisi, _Blok),
