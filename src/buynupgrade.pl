8799c261cb984f5ba83cf1054629d25103fb2fc0
hanifmz07 <13521157@std.stei.itb.ac.id>
2022-11-28 13:18:42 +0700

    fix: Add validation for upgrade

--- src/buynupgrade.pl
+++ src/buynupgrade.pl
@@ -46,8 +46,8 @@ buy :-
                 (UangNew < 0 -> 
                     write('\nKm gpunya uang yg cukup\n'), 
                     assertz(property(ID, Nama_properti, Indeks, Deskripsi_properti, Tipe, Rent, Akuisisi, Blok)),
-                    assertz(aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti))
-                ), !;
+                    assertz(aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti)), !, fail
+                );
 
                 (UangNew >= 0 ->
                     Nilai_properti_new is Nilai_properti + HargaBuy, 
@@ -62,7 +62,7 @@ buy :-
         (Pemilik == Nama -> 
             write('\nBangunan ini dah jadi punya u, ketik upgrade kalo mau upgrade y brow\n'),
             assertz(property(ID, Nama_properti, Indeks, Deskripsi_properti, Tipe, Rent, Akuisisi, Blok)),
-            assertz(aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti))
+            assertz(aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti)), !, fail
         );
 
         ((Pemilik \== 'None', Pemilik \== Nama) -> UangNew is Uang - Akuisisi,
@@ -72,7 +72,7 @@ buy :-
                             (UangNew < 0 ->  
                                 write('\nKm gpunya uang yg cukup\n'),
                                 assertz(property(ID, Nama_properti, Indeks, Deskripsi_properti, Tipe, Rent, Akuisisi, Blok)),
-                                assertz(aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti))
+                                assertz(aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti)), !, fail
                             );                                            
 
                             (UangNew >= 0 -> 
@@ -124,10 +124,24 @@ worldTour(Pemain) :-
     
 
 upgrade :-
+    retract(list_player(ListNama, Giliran)),
+    assertz(list_player(ListNama, Giliran)),
+    getElmtList(ListNama, Giliran, Nama),
+
     retract(lokasi_pemain(Nama, Indeks)),
     assertz(lokasi_pemain(Nama, Indeks)),
 
+    checkProperty(Indeks),
 
+    kepemilikan(Pemilik, ID),
+
+    (
+        Pemilik == Nama,
+        true;
+        write('Kamu tidak bisa upgrade sesuatu yang bukan punyamu! >:(\n'),
+        !, fail
+    ),
+    
     retract(property(ID, Nama_properti, Indeks, Deskripsi_properti, Tipe, Rent, Akuisisi, Blok)),
     retract(aset_pemain(Nama, Uang, Nilai_properti, Daftar_properti)),
 
